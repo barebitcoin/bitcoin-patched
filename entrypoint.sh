88e0ef8fd1f07d53f6763a1ead94616fc5210161
Bjørn Olav <bo@jalborg.com>
2024-09-26 11:22:52 +0200

    add Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+if [ -n "${UID+x}" ] && [ "${UID}" != "0" ]; then
+  usermod -u "$UID" drivechain
+fi
+
+if [ -n "${GID+x}" ] && [ "${GID}" != "0" ]; then
+  groupmod -g "$GID" drivechain
+fi
+
+echo "$0: assuming uid:gid for drivechain:drivechain of $(id -u drivechain):$(id -g drivechain)"
+
+if [ "$(echo "$1" | cut -c1)" = "-" ]; then
+  echo "$0: assuming arguments for drivechaind"
+
+  set -- drivechaind "$@"
+fi
+
+if [ "$(echo "$1" | cut -c1)" = "-" ] || [ "$1" = "drivechaind" ]; then
+  mkdir -p "$DRIVECHAIN_DATA"
+  chmod 700 "$DRIVECHAIN_DATA"
+  # Fix permissions for home dir.
+  chown -R drivechain:drivechain "$(getent passwd drivechain | cut -d: -f6)"
+  # Fix permissions for drivechain data dir.
+  chown -R drivechain:drivechain "$DRIVECHAIN_DATA"
+
+  echo "$0: setting data directory to $DRIVECHAIN_DATA"
+
+  set -- "$@" -datadir="$DRIVECHAIN_DATA"
+fi
+
+if [ "$1" = "drivechaind" ] || [ "$1" = "drivechain-cli" ] || [ "$1" = "drivechain-tx" ]; then
+  echo
+  exec gosu drivechain "$@"
+fi
+
+echo
+exec "$@"
