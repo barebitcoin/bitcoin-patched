ab205aa897faeac0368b4b102cb5c6935e514b8a
CryptAxe <cryptaxe@gmail.com>
2024-11-17 13:15:54 -0800

    Update default data dir name from Bitcoin to drivechain

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -722,19 +722,12 @@ bool HasTestOption(const ArgsManager& args, const std::string& test_option)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows:
-    //   old: C:\Users\Username\AppData\Roaming\Bitcoin
-    //   new: C:\Users\Username\AppData\Local\Bitcoin
-    // macOS: ~/Library/Application Support/Bitcoin
-    // Unix-like: ~/.bitcoin
+    // Windows: C:\Users\Username\AppData\Local\drivechain
+    // macOS: ~/Library/Application Support/drivechain
+    // Unix-like: ~/.drivechain
 #ifdef WIN32
     // Windows
-    // Check for existence of datadir in old location and keep it there
-    fs::path legacy_path = GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
-    if (fs::exists(legacy_path)) return legacy_path;
-
-    // Otherwise, fresh installs can start in the new, "proper" location
-    return GetSpecialFolderPath(CSIDL_LOCAL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_LOCAL_APPDATA) / "drivechain";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -744,10 +737,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef __APPLE__
     // macOS
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/drivechain";
 #else
     // Unix-like
-    return pathRet / ".bitcoin";
+    return pathRet / ".drivechain";
 #endif
 #endif
 }
